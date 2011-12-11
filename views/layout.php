d155b820ea002b45e1f0463cad2fb623899fcbac
Tisho Georgiev <tihomir.georgiev@gmail.com>
2011-12-11 15:39:27 +0200

    Added samurai branding, developer resources link and demo app selector.

--- views/layout.php
+++ views/layout.php
@@ -6,6 +6,28 @@
 		<?php echo (isset($_head) ? $_head : '') ?>
   </head>
   <body>
+    <header id="samurai-branding">
+      <div class="wrapper">
+        <hgroup>
+          <h1 class="logo"><a href="https://samurai.feefighters.com">Samurai by FeeFighters</a></h1>
+          <h2 class="subtitle"><span class="separator">/</span> Demo App</h2>
+        </hgroup>
+
+        <a href="https://samurai.feefighters.com/developers/php" class="developers-link"><span class="icon"></span>Developer Resources</a>
+        <div class="language">
+          <ul class="choices">
+            <li><a href="http://examples.samurai.feefighters.com/rails">Rails</a></li>
+            <li><a href="http://examples.samurai.feefighters.com/python">Python</a></li>
+            <li><a href="http://examples.samurai.feefighters.com/java">Java</a></li>
+            <li><a href="http://examples.samurai.feefighters.com/nodejs">Node.js</a></li>
+            <li><a href="http://examples.samurai.feefighters.com/dotnet">.NET</a></li>
+            <li><a href="http://examples.samurai.feefighters.com/sinatra">Sinatra</a></li>
+          </ul>
+          <a class="current" href="http://examples.samurai.feefighters.com/php">PHP</a>
+        </div>
+      </div>
+    </header>
+
     <header id="banner">
       <h1>Samurai Weapons</h1>
     </header>
